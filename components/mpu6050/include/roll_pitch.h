1d6be929ebecd3f02c4ba78836adb73098798cd0
MianMuhammad_Idrees <121511782+MianIdrees@users.noreply.github.com>
2024-07-28 06:56:06 +0500

    Update roll_pitch.h

--- components/mpu6050/include/roll_pitch.h
+++ components/mpu6050/include/roll_pitch.h
@@ -1,3 +1,29 @@
+/********************************************************************************************
+ * Project: MPU6050 ESP32 Sensor Interface
+ * Author: Muhammad Idrees
+ * 
+ * Description:
+ * This header file declares the functions required for roll and pitch calculations using
+ * complementary filters. It provides the necessary prototypes for initializing and updating
+ * angle calculations.
+ * 
+ * Author's Background:
+ * Name: Muhammad Idrees
+ * Degree: Bachelor's in Electrical and Electronics Engineering
+ * Institution: Institute of Space Technology, Islamabad
+ * 
+ * License:
+ * This header file is part of the MPU6050 interface project authored by Muhammad Idrees.
+ * It is intended for educational purposes and may be used with acknowledgment of the author.
+ * 
+ * Key Features:
+ * - Complementary filter setup and update functions.
+ * 
+ * Date: [28/7/2024]
+ ********************************************************************************************/
+
+
+
 #ifndef ROLL_PITCH_H
 #define ROLL_PITCH_H
 
