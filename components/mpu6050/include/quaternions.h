bdbf6b2d64a76324adeda24e5686b69d84f27c16
MianMuhammad_Idrees <121511782+MianIdrees@users.noreply.github.com>
2024-07-28 06:55:08 +0500

    Update quaternions.h

--- components/mpu6050/include/quaternions.h
+++ components/mpu6050/include/quaternions.h
@@ -1,3 +1,31 @@
+/********************************************************************************************
+ * Project: MPU6050 ESP32 Sensor Interface
+ * Author: Muhammad Idrees
+ * 
+ * Description:
+ * This header file defines the quaternion structure and functions for quaternion-based 
+ * angle estimation. It provides an interface for initializing, updating, and retrieving 
+ * quaternion-based roll, pitch, and yaw.
+ * 
+ * Author's Background:
+ * Name: Muhammad Idrees
+ * Degree: Bachelor's in Electrical and Electronics Engineering
+ * Institution: Institute of Space Technology, Islamabad
+ * 
+ * License:
+ * Written by Muhammad Idrees, this header file supports quaternion calculations in the 
+ * MPU6050 interface project. You are encouraged to use and adapt the code with credit 
+ * to the original author.
+ * 
+ * Key Features:
+ * - Quaternion structure and math functions.
+ * - Interfaces for roll, pitch, and yaw computation.
+ * 
+ * Date: [28/7/2024]
+ ********************************************************************************************/
+
+
+
 #ifndef QUATERNIONS_H
 #define QUATERNIONS_H
 
